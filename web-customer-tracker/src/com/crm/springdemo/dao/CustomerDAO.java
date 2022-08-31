fa06b980f71e443a1f565ab209700526e73726ae
Alex <alexandrulapshin@gmail.com>
2022-08-31 17:28:37 -0400

    Add option to save a new customer to a database

--- web-customer-tracker/src/com/crm/springdemo/dao/CustomerDAO.java
+++ web-customer-tracker/src/com/crm/springdemo/dao/CustomerDAO.java
@@ -7,5 +7,7 @@ import com.crm.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
 	
 }
--- web-customer-tracker/src/com/crm/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/crm/springdemo/dao/CustomerDAOImpl.java
@@ -35,6 +35,16 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return customers;
 	}
 
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer to database
+		currentSession.save(theCustomer);
+	}
+
 }
 
 
